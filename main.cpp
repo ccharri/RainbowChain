85b44be3739e6ec9b849da6682d57c30d6503c0e
Itszuvalex <Itszuvalex@gmail.com>
2014-02-14 15:05:33 -0500

    Checking code on new branch.  When I confirm this works, will merge into analytics.

--- main.cpp
+++ main.cpp
@@ -12,12 +12,19 @@
 #include <fstream>
 #include <sstream>
 #include <iterator>
+#include <thread>
+#include <mutex>
+#include <atomic>
+#include <vector>
+#include <time.h>
 
 using std::vector; using std::string; using std::hex;
 using std::cout; using std::endl; using std::cin;
 using std::istream_iterator; using std::istream;
 using std::ifstream; using std::stringstream;
-
+using std::thread; using std::mutex; using std::atomic;
+using std::vector;
+using std::time_t; using std::time;
 
 // SHA Rainbow Table params
 const size_t MAX_KEY_LENGTH = 8;
@@ -28,6 +35,9 @@ const size_t CHAIN_LENGTH   = 2000;
 const size_t MAX_FNAME      = 33;
 const string CHARACTER_SET  = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
+atomic<long> g_num_cracked(0);
+atomic<long> g_num_entries(0);
+mutex g_file_lock;
 
 void best_redux_func(char key[MAX_KEY_LENGTH], const char * digest, size_t step)
 {
@@ -46,10 +56,10 @@ void SHA_CIPHER_FN(char digest[SHA_OUTPUT_LEN], const char * key)
 }
 
 
-bool parseCommands(int argc, char ** argv, char filename[MAX_FNAME])
+bool parseCommands(int argc, char ** argv, char filename[MAX_FNAME], int& num_theads)
 {
 	char c; 
-  while ((c = getopt (argc, argv, "f:")) != -1)
+  while ((c = getopt (argc, argv, "f:n:")) != -1)
     switch (c)
     {
       case 'f':
@@ -63,7 +73,14 @@ bool parseCommands(int argc, char ** argv, char filename[MAX_FNAME])
           strncpy(filename, optarg, MAX_FNAME);
 
         break;
-
+      case 'n':
+        num_threads = atoi(optarg);
+        if(num_threads <= 0)
+        {
+          cout << "Invalid number of threads" << endl;
+          return true;
+        }
+        break;
       default:
      	  cout << "Invalid arguments" << endl;
         return true;
@@ -111,29 +128,65 @@ void crack_hashes(Rainbow_table <RED_FN, MAX_KEY_LEN, CIPHER_FN, CIPHER_OUTPUT_L
   // Read and try to crack each hash from the istream
   string hashstr;
 
+  g_file_lock.lock();
   while (hashstream >> hashstr)
   {
+    g_file_lock.unlock();
+
+    g_num_entries++;
+
     char hash[SHA_OUTPUT_LEN];
     hexstr_to_binary(hashstr, hash);
     string password = rtable.search(hash);
 
     if (password.length())
+    {
       cout << "Found matching password " << password << endl;
+      g_num_cracked++;
+    }
     else
       cout << "Password not found" << endl;
-  }
-}
 
+    g_file_lock.lock();
+  }
+  g_file_lock.unlock();
+}
 
 // Constructs a SHA1 rainbow table, then reads in hashes in ascii format
 // from the input stream and tries to crack them one by one
-void crack_SHA1(istream & hashstream)
+void crack_SHA1(istream & hashstream, int num_threads)
 {
+  time_t table_start, table_end, crack_start, crack_end;
+
+  time(&table_start);
   // Construct the rainbow table
   Rainbow_table <best_redux_func, MAX_KEY_LENGTH, SHA_CIPHER_FN, SHA_OUTPUT_LEN> 
     rtable(NUM_ROWS, CHAIN_LENGTH, CHARACTER_SET);
 
-  crack_hashes(rtable, hashstream);
+  time(&table_end);
+
+  cout << "Table constructed in " << table_end - table_start << " seconds." << endl;
+
+  time(&crack_start);
+
+  vector<thread> threads;
+
+  for(int i = 0; i < num_threads; ++i)
+  {
+    threads.emplace_back(crack_hashes, std::ref(rtable), std::ref(hashstream));
+  }
+
+  for(thread& t : threads)
+  {
+    t.join();
+  }
+
+  time(&crack_end);
+
+  cout << "Cracking Finished.  Total time = " << crack_end - crack_start << " seconds." << endl;
+  cout << g_num_cracked << " cracked out of " << g_num_entries << " total entries." << endl;
+  cout << ((double)g_num_cracked)/((double)g_num_entries) << "% cracked.") << endl;
+
 }
   
 
@@ -142,16 +195,17 @@ int main(int argc, char ** argv)
 {
   // Read the command line args
   char filename[MAX_FNAME] = { '\0' }; 
- 	if(parseCommands(argc, argv, filename))
+  size_t num_theads = 1;
+ 	if(parseCommands(argc, argv, filename, num_theads))
  		return 1;
 
   // If no filename provided, read from stdin
   if (*filename)
   {
     ifstream hashfstr(filename);
-    crack_SHA1(hashfstr);
+    crack_SHA1(hashfstr, num_threads);
   }
 
   else
-    crack_SHA1(cin);
+    crack_SHA1(cin, num_threads);
 }
